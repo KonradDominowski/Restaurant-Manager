6c4d2cb18b9d4a921d1635e76abff53c6b5c408d
Konrad Dominowski <zehkan@gmail.com>
2022-05-09 23:13:47 +0200

    create models 1.0

--- restaurant/manager_app/models.py
+++ restaurant/manager_app/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+class Reservation(models.Model):
+    name = models.CharField(max_length=128)
+    guest_number = models.PositiveIntegerField()
+    date_hour = models.DateTimeField(null=True)
+    table = models.ForeignKey('Table', on_delete=models.CASCADE, null=True)
+    menu = models.ForeignKey('Menu', on_delete=models.CASCADE, null=True)
+    extra_info = models.ManyToManyField('ExtraInfo', through='ReservationExtraInfo')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Table(models.Model):
+    name = models.CharField(max_length=64)
+    capacity = models.IntegerField()
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=128)
+    prepared = models.BooleanField()
+    dishes = models.ManyToManyField('Dish')
+    price = models.FloatField()
+    active = models.BooleanField()
+
+
+DISH_CATEGORY = (
+    ('Przystawka', 'Przystawka',),
+    ('Zupa', 'Zupa',),
+    ('Danie główne', 'Danie główne',),
+    ('Deser', 'Deser',),
+)
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=128)
+    category = models.CharField(max_length=32, choices=DISH_CATEGORY)
+    price = models.FloatField()
+
+
+class ExtraInfo(models.Model):
+    vegetarian = models.BooleanField()
+    vegan = models.BooleanField()
+    celiac = models.BooleanField()
+    peanut_allergy = models.BooleanField()
+    dairy = models.BooleanField()
+    child_seat = models.BooleanField()
+
+
+class ReservationExtraInfo(models.Model):
+    reservation = models.ForeignKey('Reservation', on_delete=models.CASCADE)
+    info = models.ForeignKey('ExtraInfo', on_delete=models.CASCADE)
+    amount = models.IntegerField()
