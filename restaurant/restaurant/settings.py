1793f881f913e52636983e3d3323471aefd07872
Konrad Dominowski <zehkan@gmail.com>
2022-06-16 16:48:25 +0200

    changed secret key

--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -11,15 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+from .secret_key import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pp_=!6$teh_!77^pobuamq9=bphufz-v+_4h%a%wd*xb04v28x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
