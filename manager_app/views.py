680419f730b5c81013e835fd3b2d87534f388a86
Konrad Dominowski <zehkan@gmail.com>
2023-07-08 13:43:11 +0200

    commit

--- manager_app/views.py
+++ manager_app/views.py
@@ -11,8 +11,14 @@ from django.utils.translation import gettext as _
 from django.views import View
 from django.views.generic import CreateView, ListView
 
-from .forms import CreateReservationForm, SelectTableForm, SelectMenuForm, ExtraInfoForm, ChangeGuestNumberForm, \
-    DateRangeForm
+from .forms import (
+    CreateReservationForm,
+    SelectTableForm,
+    SelectMenuForm,
+    ExtraInfoForm,
+    ChangeGuestNumberForm,
+    DateRangeForm,
+)
 from .models import Reservation, Dish, Menu, ExtraInfo
 
 
@@ -23,41 +29,44 @@ def daterange(start_date, end_date):
 
 def get_dishes_by_type():
     dishes = {
-        'przystawka_grupowa': Dish.objects.filter(category='Przystawka grupowa').order_by('name'),
-        'przystawka': Dish.objects.filter(category='Przystawka').order_by('name'),
-        'zupa': Dish.objects.filter(category='Zupa').order_by('name'),
-        'danie_glowne': Dish.objects.filter(category='Danie główne').order_by('name'),
-        'deser': Dish.objects.filter(category='Deser').order_by('name'),
+        "przystawka_grupowa": Dish.objects.filter(
+            category="Przystawka grupowa"
+        ).order_by("name"),
+        "przystawka": Dish.objects.filter(category="Przystawka").order_by("name"),
+        "zupa": Dish.objects.filter(category="Zupa").order_by("name"),
+        "danie_glowne": Dish.objects.filter(category="Danie główne").order_by("name"),
+        "deser": Dish.objects.filter(category="Deser").order_by("name"),
     }
     return dishes
 
 
 class IndexView(View):
     def get(self, request):
+
         today = date.today()
-        reservations = Reservation.objects.filter(date=today).order_by('hour')
+        reservations = Reservation.objects.filter(date=today).order_by("hour")
         all_guests = sum(i.guest_number for i in reservations)
         ctx = {
-            'date': today,
-            'reservations': reservations,
-            'amount': reservations.count(),
-            'all_guests': all_guests
+            "date": today,
+            "reservations": reservations,
+            "amount": reservations.count(),
+            "all_guests": all_guests,
         }
-        return render(request, 'index.html', ctx)
+        return render(request, "index.html", ctx)
 
 
 class CreateDishView(CreateView):
     model = Dish
-    fields = '__all__'
-    success_url = '/menu/dish/list/'
-    template_name = 'dish-add.html'
+    fields = "__all__"
+    success_url = "/menu/dish/list/"
+    template_name = "dish-add.html"
 
 
 class DishListView(ListView):
     """This view displays all dishes in the database, sorted by their category."""
 
     model = Dish
-    template_name = 'dish-list.html'
+    template_name = "dish-list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -67,38 +76,36 @@ class DishListView(ListView):
 
 
 class CreateReservationView(View):
-    """This view is for adding a reservation. """
+    """This view is for adding a reservation."""
 
     def get(self, request):
         form = CreateReservationForm()
-        if request.GET.get('date'):
-            raw_date = request.GET.get('date').split(',')
+        if request.GET.get("date"):
+            raw_date = request.GET.get("date").split(",")
             res_date = date(int(raw_date[0]), int(raw_date[1]), int(raw_date[2]))
-            form = CreateReservationForm(initial={'date': res_date})
-        ctx = {
-            'form': form
-        }
+            form = CreateReservationForm(initial={"date": res_date})
+        ctx = {"form": form}
         try:
-            ctx['message'] = self.request.session['message']
-            del self.request.session['message']
+            ctx["message"] = self.request.session["message"]
+            del self.request.session["message"]
         except KeyError:
             pass
-        return render(request, 'reservations-add.html', ctx)
+        return render(request, "reservations-add.html", ctx)
 
     def post(self, request):
         form = CreateReservationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect(reverse('browse-reservations'))
-        return render(request, 'reservations-add.html', {'form': form})
+            return redirect(reverse("browse-reservations"))
+        return render(request, "reservations-add.html", {"form": form})
 
 
 class BrowseReservationsView(ListView):
     """Shows all reservations scheduled in the following 2 weeks."""
 
     model = Reservation
-    template_name = 'reservations-browse.html'
-    context_object_name = 'reservations'
+    template_name = "reservations-browse.html"
+    context_object_name = "reservations"
 
     def get_context_data(self, start_date=None, end_date=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -108,19 +115,24 @@ class BrowseReservationsView(ListView):
         if not end_date:
             end_date = start_date + timedelta(days=14)
 
-        context['form'] = DateRangeForm(initial={'start_date': start_date, 'end_date': end_date})
+        context["form"] = DateRangeForm(
+            initial={"start_date": start_date, "end_date": end_date}
+        )
 
         if start_date == end_date:
-            queryset = Reservation.objects.filter(date__gte=start_date).order_by('date', 'hour')
+            queryset = Reservation.objects.filter(date__gte=start_date).order_by(
+                "date", "hour"
+            )
         else:
-            queryset = Reservation.objects.filter(date__gte=start_date,
-                                                  date__lte=(end_date + timedelta(days=1))).order_by('date', 'hour')
-        context['reservations'] = queryset
-        context['date_range'] = daterange(start_date, end_date + timedelta(days=1))
-        context['res_date'] = sorted(list(set([res.date for res in queryset])))
+            queryset = Reservation.objects.filter(
+                date__gte=start_date, date__lte=(end_date + timedelta(days=1))
+            ).order_by("date", "hour")
+        context["reservations"] = queryset
+        context["date_range"] = daterange(start_date, end_date + timedelta(days=1))
+        context["res_date"] = sorted(list(set([res.date for res in queryset])))
         try:
-            context['message'] = self.request.session['message']
-            del self.request.session['message']
+            context["message"] = self.request.session["message"]
+            del self.request.session["message"]
         except KeyError:
             pass
         return context
@@ -134,7 +146,7 @@ class BrowseReservationsView(ListView):
             # it's better to do a cheap query than to load the unpaginated
             # queryset in memory.
             if self.get_paginate_by(self.object_list) is not None and hasattr(
-                    self.object_list, "exists"
+                self.object_list, "exists"
             ):
                 is_empty = not self.object_list.exists()
             else:
@@ -150,12 +162,12 @@ class BrowseReservationsView(ListView):
         form = DateRangeForm(request.POST)
 
         if form.is_valid():
-            start_date = form.cleaned_data['start_date']
-            end_date = form.cleaned_data['end_date']
+            start_date = form.cleaned_data["start_date"]
+            end_date = form.cleaned_data["end_date"]
             context = self.get_context_data(start_date=start_date, end_date=end_date)
 
             return self.render_to_response(context)
-        return render(request, 'reservations-browse.html', {'form': form})
+        return render(request, "reservations-browse.html", {"form": form})
 
 
 # TODO - date, hour, name change, notes don't show, add form for them
@@ -168,36 +180,40 @@ class ReservationDetailView(View):
     def build_context(self, request, res_id):
         reservation = Reservation.objects.get(id=res_id)
 
-        if request.method == 'GET':
+        if request.method == "GET":
             table_form = SelectTableForm(instance=reservation)
             guest_number_form = ChangeGuestNumberForm(instance=reservation)
             menu_form = SelectMenuForm(instance=reservation)
         else:
             table_form = SelectTableForm(request.POST, instance=reservation)
-            guest_number_form = ChangeGuestNumberForm(request.POST, instance=reservation)
+            guest_number_form = ChangeGuestNumberForm(
+                request.POST, instance=reservation
+            )
             menu_form = SelectMenuForm(request.POST, instance=reservation)
 
         ctx = {
-            'res': reservation,
-            'table_form': table_form,
-            'menu_form': menu_form,
-            'guest_number_form': guest_number_form,
+            "res": reservation,
+            "table_form": table_form,
+            "menu_form": menu_form,
+            "guest_number_form": guest_number_form,
         }
 
         try:
             extra_info = ExtraInfo.objects.get(reservation=reservation)
-            ctx['extra_info'] = extra_info
-            ctx['extra_info_form'] = ExtraInfoForm(instance=extra_info)
-            ctx['has_extra_info'] = sum(value for name, value in extra_info.get_fields()[1:]) > 0
+            ctx["extra_info"] = extra_info
+            ctx["extra_info_form"] = ExtraInfoForm(instance=extra_info)
+            ctx["has_extra_info"] = (
+                sum(value for name, value in extra_info.get_fields()[1:]) > 0
+            )
         except ExtraInfo.DoesNotExist:
-            ctx['extra_info_form'] = ExtraInfoForm(initial={'reservation': reservation})
-            ctx['has_extra_info'] = False
+            ctx["extra_info_form"] = ExtraInfoForm(initial={"reservation": reservation})
+            ctx["has_extra_info"] = False
 
         return ctx
 
     def get(self, request, res_id):
         ctx = self.build_context(request, res_id)
-        return render(request, 'reservations-details.html', ctx)
+        return render(request, "reservations-details.html", ctx)
 
     def post(self, request, res_id):
         reservation = Reservation.objects.get(id=res_id)
@@ -214,7 +230,7 @@ class ReservationDetailView(View):
 
         ctx = self.build_context(request, res_id)
 
-        return render(request, 'reservations-details.html', ctx)
+        return render(request, "reservations-details.html", ctx)
 
 
 class RemoveMenuFromReservation(View):
@@ -224,7 +240,7 @@ class RemoveMenuFromReservation(View):
         reservation = Reservation.objects.get(id=res_id)
         reservation.menu = None
         reservation.save()
-        return redirect(reverse('reservation-details', kwargs={'res_id': res_id}))
+        return redirect(reverse("reservation-details", kwargs={"res_id": res_id}))
 
 
 class SaveInfoToReservation(View):
@@ -240,7 +256,7 @@ class SaveInfoToReservation(View):
 
         if form.is_valid():
             form.save()
-        return redirect(reverse('reservation-details', kwargs={'res_id': res_id}))
+        return redirect(reverse("reservation-details", kwargs={"res_id": res_id}))
 
 
 class DeleteReservation(View):
@@ -248,9 +264,11 @@ class DeleteReservation(View):
 
     def post(self, request, res_id):
         reservation = Reservation.objects.get(id=res_id)
-        request.session['message'] = f'Usunięto rezerwację <strong>{reservation}</strong>'
+        request.session[
+            "message"
+        ] = f"Usunięto rezerwację <strong>{reservation}</strong>"
         reservation.delete()
-        return redirect(reverse('browse-reservations'))
+        return redirect(reverse("browse-reservations"))
 
 
 class ReservationsSearchView(View):
@@ -258,75 +276,80 @@ class ReservationsSearchView(View):
 
     def get(self, request):
         reservations = Reservation.objects.all()
-        ctx = {
-            'reservations': reservations
-        }
-        return render(request, 'reservations-search.html', ctx)
+        ctx = {"reservations": reservations}
+        return render(request, "reservations-search.html", ctx)
 
     def post(self, request):
-        search = request.POST.get('search')
+        search = request.POST.get("search")
         try:
-            reservations = Reservation.objects.filter(name__icontains=search).order_by('date')
-            ctx = {
-                'reservations': reservations
-            }
+            reservations = Reservation.objects.filter(name__icontains=search).order_by(
+                "date"
+            )
+            ctx = {"reservations": reservations}
         except Reservation.DoesNotExist:
-            return HttpResponse('Nie istnieją takie rezerwacje')
-        return render(request, 'reservations-search.html', ctx)
+            return HttpResponse("Nie istnieją takie rezerwacje")
+        return render(request, "reservations-search.html", ctx)
 
 
 class CreateMenuView(LoginRequiredMixin, View):
     def get(self, request):
         ctx = get_dishes_by_type()
-        return render(request, 'menu-add.html', ctx)
+        return render(request, "menu-add.html", ctx)
 
     def post(self, request):
-        name = request.POST.get('name')
-        price = int(request.POST.get('price'))
-        dishes = [name for name, value in request.POST.items() if value == 'on']
+        name = request.POST.get("name")
+        price = int(request.POST.get("price"))
+        dishes = [name for name, value in request.POST.items() if value == "on"]
         dishes_query_set = [Dish.objects.get(name=name) for name in dishes]
         ctx = get_dishes_by_type()
-        ctx['name'] = name
-        ctx['price'] = price
-        ctx['dishes'] = dishes_query_set
-        if name != '' and price > 0:
+        ctx["name"] = name
+        ctx["price"] = price
+        ctx["dishes"] = dishes_query_set
+        if name != "" and price > 0:
             try:
-                new_menu = Menu(name=name, prepared=True,
-                                price=price, active=True)
+                new_menu = Menu(name=name, prepared=True, price=price, active=True)
                 new_menu.save()
                 for dish in dishes_query_set:
                     new_menu.dishes.add(dish)
-                return redirect(reverse_lazy('menu-details', kwargs={'menu_id': new_menu.id}))
+                return redirect(
+                    reverse_lazy("menu-details", kwargs={"menu_id": new_menu.id})
+                )
             except IntegrityError:
-                ctx['message'] = 'Menu o takiej nazwie już istnieje'
-                return render(request, 'menu-add.html', ctx)
+                ctx["message"] = "Menu o takiej nazwie już istnieje"
+                return render(request, "menu-add.html", ctx)
         else:
-            ctx['message'] = 'Cena musi być powyżej 0'
-            return render(request, 'menu-add.html', ctx)
+            ctx["message"] = "Cena musi być powyżej 0"
+            return render(request, "menu-add.html", ctx)
 
 
 class EditMenuView(View):
     def get(self, request, menu_id):
         ctx = get_dishes_by_type()
         menu = Menu.objects.get(id=menu_id)
-        ctx['menu'] = menu
-        return render(request, 'menu-edit.html', ctx)
+        ctx["menu"] = menu
+        return render(request, "menu-edit.html", ctx)
 
     def post(self, request, menu_id):
         menu_to_update = Menu.objects.get(id=menu_id)
-        name = request.POST.get('name')
-        price = int(float(request.POST.get('price')))
-        dishes = [name for name, value in request.POST.items() if value == 'on']
+        name = request.POST.get("name")
+        price = int(float(request.POST.get("price")))
+        dishes = [name for name, value in request.POST.items() if value == "on"]
         new_dishes_query_set = [Dish.objects.get(name=name) for name in dishes]
-        if name != '' and price > 0:
+        if name != "" and price > 0:
             menu_to_update.name = name
             menu_to_update.price = price
             # menu_to_update.dishes.clear()  # Pierwsze podejście to usunięcie wszystkich dań,
             # for dish in new_dishes_query_set:  # a potem dodanie wszystkich z formularza
             #     menu_to_update.dishes.add(dish)
 
-            for dish in menu_to_update.dishes.all():  # Drugie podejście to usunięcie tylko dań,
-                if dish not in new_dishes_query_set:  # których nie ma w formularzu, a potem dodanie brakujących
+            for (
+                dish
+            ) in (
+                menu_to_update.dishes.all()
+            ):  # Drugie podejście to usunięcie tylko dań,
+                if (
+                    dish not in new_dishes_query_set
+                ):  # których nie ma w formularzu, a potem dodanie brakujących
                     menu_to_update.dishes.remove(dish)
 
             for dish in new_dishes_query_set:
@@ -334,39 +357,43 @@ class EditMenuView(View):
                     menu_to_update.dishes.add(dish)
 
             menu_to_update.save()
-            return redirect(reverse_lazy('menu-details', kwargs={'menu_id': menu_to_update.id}))
+            return redirect(
+                reverse_lazy("menu-details", kwargs={"menu_id": menu_to_update.id})
+            )
 
 
 class ArchiveMenuView(View):
     def get(self, request, menu_id):
         menu = Menu.objects.get(id=menu_id)
         menu.active = False
-        request.session['message'] = f'Menu <strong>{menu}</strong> zostało zarchiwizowane'
+        request.session[
+            "message"
+        ] = f"Menu <strong>{menu}</strong> zostało zarchiwizowane"
         menu.save()
-        return redirect(reverse('menu-list'))
+        return redirect(reverse("menu-list"))
 
 
 class DetailMenuView(ListView):
     model = Menu
-    context_object_name = 'menu'
-    template_name = 'menu-details.html'
+    context_object_name = "menu"
+    template_name = "menu-details.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = Menu.objects.get(id=self.kwargs['menu_id'])
+        context["menu"] = Menu.objects.get(id=self.kwargs["menu_id"])
         return context
 
 
 class MenuListView(ListView):
     model = Menu
-    template_name = 'menu-list.html'
+    template_name = "menu-list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menus'] = Menu.objects.filter(active=True).order_by('name')
+        context["menus"] = Menu.objects.filter(active=True).order_by("name")
         try:
-            context['message'] = self.request.session['message']
-            del self.request.session['message']
+            context["message"] = self.request.session["message"]
+            del self.request.session["message"]
         except KeyError:
             pass
         return context
@@ -375,19 +402,17 @@ class MenuListView(ListView):
 class SignUpView(View):
     def get(self, request):
         form = UserCreationForm()
-        ctx = {
-            'form': form
-        }
-        return render(request, 'signup.html', ctx)
+        ctx = {"form": form}
+        return render(request, "signup.html", ctx)
 
     def post(self, request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             print(form.cleaned_data)
             form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect(reverse('index-view'))
-        return redirect('signup')
+            return redirect(reverse("index-view"))
+        return redirect("signup")
