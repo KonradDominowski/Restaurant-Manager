1b863fedf99a264feef53d6be350c463ec144252
Konrad Dominowski <zehkan@gmail.com>
2023-07-08 14:16:33 +0200

    Update settings.py

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['restaurantmanagementapp.herokuapp.com']
+ALLOWED_HOSTS = ['13.50.30.127', 'localhost']
 
 # Application definition
 
